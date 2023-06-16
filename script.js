66733fdaaabe2658cbdc5d5582cbcdbdf0a4c695
Abraham Avendaño Vercelli <abrahamavever@gmail.com>
2023-06-16 18:14:44 -0400

    git commit -m "Agregar elemento meta de viewport
    Se agregó el elemento meta de viewport en el archivo index.html para especificar las instrucciones al navegador sobre cómo controlar las dimensiones y la escala de la página en diferentes dispositivos. Esto soluciona el error 'A 'viewport' meta element was not specified'."

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let trotarDuracionInicial = 70;
 let trotarDuracionIncremento = 10;
 let trotarNivel = 1;
+let cuentaRegresiva;
 
 // Función para incrementar el nivel de trotar
 function aumentarNivelTrotar() {
@@ -11,5 +12,21 @@ function aumentarNivelTrotar() {
     document.getElementById("trotar-duracion").textContent = trotarDuracionInicial;
     console.log("¡Has subido de nivel en trotar! Nivel actual: " + trotarNivel);
     console.log("Nueva duración de trotar: " + trotarDuracionInicial + " segundos");
+  }
+
+  function empezarCuentaRegresiva() {
+    const duracionInicial = parseInt(document.getElementById("trotar-duracion").textContent);
+    let segundosRestantes = duracionInicial;
+    document.getElementById("trotar-duracion").textContent = segundosRestantes;
+  
+    cuentaRegresiva = setInterval(() => {
+      segundosRestantes--;
+      document.getElementById("trotar-duracion").textContent = segundosRestantes;
+  
+      if (segundosRestantes <= 0) {
+        clearInterval(cuentaRegresiva);
+        alert("¡Tiempo agotado!");
+      }
+    }, 1000);
   }
 
