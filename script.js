11ff2ae029f7912fc827a2728e17cea89f1a1eb6
Abraham Avendaño Vercelli <abrahamavever@gmail.com>
2023-06-20 13:58:44 -0400

    feat: Refactorización del código para utilizar una clase TrotarManager
    Se refactorizó el código para utilizar una clase llamada TrotarManager que encapsula la lógica de gestión de la acción "Trotar". Esto proporciona una estructura más organizada y modular al código. Además, se actualizaron las funciones de control de eventos para llamar a los métodos de la instancia de TrotarManager.
    
    Los cambios incluyen:
    - Creación de la clase TrotarManager para gestionar el nivel, duración y cuenta regresiva de la acción "Trotar".
    - Uso de métodos de la instancia de TrotarManager en lugar de funciones globales.
    - Mejoras en la legibilidad y estructura del código.
    
    Los métodos disponibles son:
    - aumentarNivelTrotar(): Incrementa el nivel de la acción "Trotar".
    - bajarNivelTrotar(): Reduce el nivel de la acción "Trotar".
    - empezarCuentaRegresiva(): Inicia la cuenta regresiva para la acción "Trotar".
    - almacenarDatos(): Almacena los datos de la acción "Trotar" en el almacenamiento local.
    - mostrarDatosAlmacenados(): Muestra los datos almacenados de la acción "Trotar" en la consola.
    
    Este commit refleja los cambios realizados en el código y mejora la estructura y mantenibilidad del proyecto.

--- script.js
+++ script.js
@@ -1,104 +1,131 @@
-const trotarDuracionInicial = 70;
-const trotarDuracionIncremento = 10;
-let trotarNivel = 1;
-let cuentaRegresiva;
-const sonidoAlerta = new Audio('alerta.mp3');
-let nivelGuardado = localStorage.getItem("nivelTrotar");
+class TrotarManager {
+    constructor() {
+      this.trotarDuracionInicial = 70;
+      this.trotarDuracionIncremento = 10;
+      this.trotarNivel = 1;
+      this.cuentaRegresiva = null;
+      this.sonidoAlerta = new Audio('alerta.mp3');
+      this.nivelGuardado = localStorage.getItem("nivelTrotar");
+  
+      if (this.nivelGuardado) {
+        this.trotarNivel = parseInt(this.nivelGuardado);
+      } else {
+        this.guardarNivelTrotar();
+        this.nivelGuardado = this.trotarNivel.toString();
+      }
+  
+      this.actualizarDatosTrotar();
+    }
+  
+    guardarNivelTrotar() {
+      localStorage.setItem("nivelTrotar", this.trotarNivel.toString());
+    }
+  
+    actualizarDatosTrotar() {
+      document.getElementById("trotar-nivel").textContent = this.trotarNivel;
+      document.getElementById("trotar-duracion").textContent = this.calcularDuracionTrotar();
+    }
+  
+    calcularDuracionTrotar() {
+      return this.trotarDuracionInicial + (this.trotarNivel - 1) * this.trotarDuracionIncremento;
+    }
+  
+    aumentarNivelTrotar() {
+      this.trotarNivel++;
+      this.guardarNivelTrotar();
+      this.actualizarDatosTrotar();
+      console.log(`¡Has subido de nivel en trotar! Nivel actual: ${this.trotarNivel}`);
+      console.log(`Nueva duración de trotar: ${this.calcularDuracionTrotar()} segundos`);
+    }
+  
+    bajarNivelTrotar() {
+      if (this.trotarNivel > 1) {
+        this.trotarNivel--;
+        this.guardarNivelTrotar();
+        this.actualizarDatosTrotar();
+        console.log(`¡Has bajado de nivel en trotar! Nivel actual: ${this.trotarNivel}`);
+        console.log(`Nueva duración de trotar: ${this.calcularDuracionTrotar()} segundos`);
+      }
+    }
+  
+    empezarCuentaRegresiva() {
+      const duracionInicial = this.calcularDuracionTrotar();
+      let segundosRestantes = duracionInicial;
+      document.getElementById("trotar-duracion").textContent = segundosRestantes;
+  
+      this.cuentaRegresiva = setInterval(() => {
+        segundosRestantes--;
+        document.getElementById("trotar-duracion").textContent = segundosRestantes;
+  
+        if (segundosRestantes <= 0) {
+          clearInterval(this.cuentaRegresiva);
+          this.reproducirSonidoAlerta();
+          this.almacenarDatos("Trotar");
+          this.reiniciarCuentaRegresiva();
+        }
+      }, 1000);
+    }
+  
+    reproducirSonidoAlerta() {
+      this.sonidoAlerta.play();
+      setTimeout(() => {
+        this.reiniciarCuentaRegresiva();
+      }, 2000);
+    }
+  
+    reiniciarCuentaRegresiva() {
+      clearInterval(this.cuentaRegresiva);
+      document.getElementById("trotar-duracion").textContent = this.calcularDuracionTrotar();
+    }
+  
+    almacenarDatos(accion) {
+      const fechaHora = new Date().toLocaleString();
+      const datos = {
+        nivel: this.trotarNivel,
+        tiempo: this.calcularDuracionTrotar(),
+        accion: accion,
+        fechaHora: fechaHora
+      };
+  
+      const datosPrevios = JSON.parse(localStorage.getItem("datos")) || [];
+      datosPrevios.push(datos);
+      localStorage.setItem("datos", JSON.stringify(datosPrevios));
+    }
+  
+    mostrarDatosAlmacenados() {
+      const datosPrevios = JSON.parse(localStorage.getItem("datos"));
+      if (datosPrevios) {
+        datosPrevios.forEach((datos) => {
+          console.log(`Nivel: ${datos.nivel}`);
+          console.log(`Tiempo: ${datos.tiempo} segundos`);
+          console.log(`Acción: ${datos.accion}`);
+          console.log(`Fecha y Hora: ${datos.fechaHora}`);
+          console.log("------------------");
+        });
+      } else {
+        console.log("No hay datos almacenados.");
+      }
+    }
+  }
 
-if (nivelGuardado) {
-  trotarNivel = parseInt(nivelGuardado);
-} else {
-  guardarNivelTrotar();
-  nivelGuardado = trotarNivel.toString();
-}
-
-actualizarDatosTrotar();
-
-function guardarNivelTrotar() {
-  localStorage.setItem("nivelTrotar", trotarNivel.toString());
-}
-
-function actualizarDatosTrotar() {
-  document.getElementById("trotar-nivel").textContent = trotarNivel;
-  document.getElementById("trotar-duracion").textContent = calcularDuracionTrotar();
-}
-
-function calcularDuracionTrotar() {
-  return trotarDuracionInicial + (trotarNivel - 1) * trotarDuracionIncremento;
-}
+  const trotarManager = new TrotarManager();
 
 function aumentarNivelTrotar() {
-  trotarNivel++;
-  guardarNivelTrotar();
-  actualizarDatosTrotar();
-  console.log(`¡Has subido de nivel en trotar! Nivel actual: ${trotarNivel}`);
-  console.log(`Nueva duración de trotar: ${calcularDuracionTrotar()} segundos`);
+  trotarManager.aumentarNivelTrotar();
 }
 
 function bajarNivelTrotar() {
-  if (trotarNivel > 1) {
-    trotarNivel--;
-    guardarNivelTrotar();
-    actualizarDatosTrotar();
-    console.log(`¡Has bajado de nivel en trotar! Nivel actual: ${trotarNivel}`);
-    console.log(`Nueva duración de trotar: ${calcularDuracionTrotar()} segundos`);
-  }
+  trotarManager.bajarNivelTrotar();
 }
 
 function empezarCuentaRegresiva() {
-  const duracionInicial = calcularDuracionTrotar();
-  let segundosRestantes = duracionInicial;
-  document.getElementById("trotar-duracion").textContent = segundosRestantes;
-
-  cuentaRegresiva = setInterval(() => {
-    segundosRestantes--;
-    document.getElementById("trotar-duracion").textContent = segundosRestantes;
-
-    if (segundosRestantes <= 0) {
-      clearInterval(cuentaRegresiva);
-      reproducirSonidoAlerta();
-      almacenarDatos(trotarNivel, duracionInicial, "Trotar");
-      reiniciarCuentaRegresiva();
-    }
-  }, 1000);
+  trotarManager.empezarCuentaRegresiva();
 }
 
-function reproducirSonidoAlerta() {
-  sonidoAlerta.play();
-  setTimeout(() => {
-    reiniciarCuentaRegresiva();
-  }, 2000); // Espera 2 segundos antes de reiniciar la cuenta regresiva
-}
-
-function reiniciarCuentaRegresiva() {
-  clearInterval(cuentaRegresiva);
-  document.getElementById("trotar-duracion").textContent = calcularDuracionTrotar();
-}
-
-function almacenarDatos(nivel, tiempo, accion) {
-  const fechaHora = new Date().toLocaleString();
-  const datos = {
-    nivel: nivel,
-    tiempo: tiempo,
-    accion: accion,
-    fechaHora: fechaHora
-  };
-  const datosPrevios = JSON.parse(localStorage.getItem("datos")) || [];
-  datosPrevios.push(datos);
-  localStorage.setItem("datos", JSON.stringify(datosPrevios));
+function almacenarDatos() {
+  trotarManager.almacenarDatos("Trotar");
 }
 
 function mostrarDatosAlmacenados() {
-  const datosPrevios = JSON.parse(localStorage.getItem("datos"));
-  if (datosPrevios) {
-    datosPrevios.forEach((datos) => {
-      console.log(`Nivel: ${datos.nivel}`);
-      console.log(`Tiempo: ${datos.tiempo} segundos`);
-      console.log(`Acción: ${datos.accion}`);
-      console.log(`Fecha y Hora: ${datos.fechaHora}`);
-      console.log("------------------");
-    });
-  } else {
-    console.log("No hay datos almacenados.");
-  }
+  trotarManager.mostrarDatosAlmacenados();
 }
