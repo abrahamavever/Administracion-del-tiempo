01deba4164d649c74e49b68ca3dba8df8590c19c
Abraham Avendaño Vercelli <abrahamavever@gmail.com>
2023-06-20 15:45:01 -0400

    git commit -m "Mejora de la estética y corrección de errores en la aplicación de administración de tiempo"

--- script.js
+++ script.js
@@ -6,38 +6,38 @@ class ActividadManager {
       this.actividadCuentaRegresiva = null;
       this.actividadSonidoAlerta = new Audio('alerta.mp3');
       this.actividadNivelGuardado = localStorage.getItem(`nivel${actividadNombre}`);
-  
+
       if (this.actividadNivelGuardado) {
         this.actividadNivel = parseInt(this.actividadNivelGuardado);
       } else {
         this.guardarNivelActividad(actividadNombre);
         this.actividadNivelGuardado = this.actividadNivel.toString();
       }
-  
+
       this.actualizarDatosActividad(actividadNombre);
     }
-  
+
     guardarNivelActividad(actividadNombre) {
       localStorage.setItem(`nivel${actividadNombre}`, this.actividadNivel.toString());
     }
-  
+
     actualizarDatosActividad(actividadNombre) {
       const nivelElement = document.getElementById(`${actividadNombre}-nivel`);
       const duracionElement = document.getElementById(`${actividadNombre}-duracion`);
-  
+
       if (nivelElement) {
         nivelElement.textContent = this.actividadNivel;
       }
-  
+
       if (duracionElement) {
         duracionElement.textContent = this.calcularDuracionActividad();
       }
     }
-  
+
     calcularDuracionActividad() {
       return this.actividadDuracionInicial + (this.actividadNivel - 1) * this.actividadDuracionIncremento;
     }
-  
+
     aumentarNivelActividad(actividadNombre) {
       this.actividadNivel++;
       this.guardarNivelActividad(actividadNombre);
@@ -45,7 +45,7 @@ class ActividadManager {
       console.log(`¡Has subido de nivel en ${actividadNombre}! Nivel actual: ${this.actividadNivel}`);
       console.log(`Nueva duración de ${actividadNombre}: ${this.calcularDuracionActividad()} segundos`);
     }
-  
+
     bajarNivelActividad(actividadNombre) {
       if (this.actividadNivel > 1) {
         this.actividadNivel--;
@@ -55,22 +55,22 @@ class ActividadManager {
         console.log(`Nueva duración de ${actividadNombre}: ${this.calcularDuracionActividad()} segundos`);
       }
     }
-  
+
     empezarCuentaRegresiva(actividadNombre) {
       const duracionInicial = this.calcularDuracionActividad();
       let segundosRestantes = duracionInicial;
       const duracionElement = document.getElementById(`${actividadNombre}-duracion`);
-  
+
       if (duracionElement) {
         duracionElement.textContent = segundosRestantes;
       }
-  
+
       this.actividadCuentaRegresiva = setInterval(() => {
         segundosRestantes--;
         if (duracionElement) {
           duracionElement.textContent = segundosRestantes;
         }
-  
+
         if (segundosRestantes <= 0) {
           clearInterval(this.actividadCuentaRegresiva);
           this.reproducirSonidoAlerta();
@@ -79,23 +79,23 @@ class ActividadManager {
         }
       }, 1000);
     }
-  
+
     reproducirSonidoAlerta(actividadNombre) {
       this.actividadSonidoAlerta.play();
       setTimeout(() => {
         this.reiniciarCuentaRegresiva(actividadNombre);
       }, 2000);
     }
-  
+
     reiniciarCuentaRegresiva(actividadNombre) {
       clearInterval(this.actividadCuentaRegresiva);
       const duracionElement = document.getElementById(`${actividadNombre}-duracion`);
-  
+
       if (duracionElement) {
         duracionElement.textContent = this.calcularDuracionActividad();
       }
     }
-  
+
     almacenarDatos(accion) {
       const fechaHora = new Date().toLocaleString();
       const datos = {
@@ -104,77 +104,87 @@ class ActividadManager {
         accion: accion,
         fechaHora: fechaHora
       };
-  
+
       const datosPrevios = JSON.parse(localStorage.getItem("datos")) || [];
       datosPrevios.push(datos);
       localStorage.setItem("datos", JSON.stringify(datosPrevios));
     }
-  
-    mostrarDatosAlmacenados() {
-      const datosPrevios = JSON.parse(localStorage.getItem("datos"));
-      if (datosPrevios) {
-        datosPrevios.forEach((datos) => {
-          console.log(`Nivel: ${datos.nivel}`);
-          console.log(`Tiempo: ${datos.tiempo} segundos`);
-          console.log(`Acción: ${datos.accion}`);
-          console.log(`Fecha y Hora: ${datos.fechaHora}`);
-          console.log("------------------");
-        });
-      } else {
-        console.log("No hay datos almacenados.");
-      }
-    }
   }
-  
+
   class TrotarManager extends ActividadManager {
     constructor() {
       super(70, 10, "trotar");
     }
   }
-  
+
   class LecturaManager extends ActividadManager {
     constructor() {
       super(10, 5, "lectura");
     }
   }
-  
+
   const trotarManager = new TrotarManager();
   const lecturaManager = new LecturaManager();
-  
+
   function aumentarNivelTrotar() {
     trotarManager.aumentarNivelActividad("trotar");
   }
-  
+
   function bajarNivelTrotar() {
     trotarManager.bajarNivelActividad("trotar");
   }
-  
+
   function empezarCuentaRegresiva() {
     trotarManager.empezarCuentaRegresiva("trotar");
   }
-  
+
   function aumentarNivelLectura() {
     lecturaManager.aumentarNivelActividad("lectura");
   }
-  
+
   function bajarNivelLectura() {
     lecturaManager.bajarNivelActividad("lectura");
   }
-  
+
   function empezarCuentaRegresivaLectura() {
     lecturaManager.empezarCuentaRegresiva("lectura");
   }
-  
+
   function almacenarDatos() {
     trotarManager.almacenarDatos("Trotar");
   }
-  
+
   function almacenarDatosLectura() {
     lecturaManager.almacenarDatos("Lectura");
   }
-  
-  function mostrarDatosAlmacenados() {
-    trotarManager.mostrarDatosAlmacenados();
-    lecturaManager.mostrarDatosAlmacenados();
+  function limpiarDatos() {
+    localStorage.removeItem("datos");
+    mostrarDatosAlmacenados();
   }
   
+
+  function mostrarDatosAlmacenados() {
+    const datosPrevios = JSON.parse(localStorage.getItem("datos"));
+    const datosTableBody = document.getElementById("datos-almacenados");
+
+    datosTableBody.innerHTML = ""; // Limpiamos el contenido existente en la tabla
+
+    if (datosPrevios) {
+      datosPrevios.forEach((datos) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+          <td>${datos.nivel}</td>
+          <td>${datos.tiempo}</td>
+          <td>${datos.accion}</td>
+          <td>${datos.fechaHora}</td>
+        `;
+        datosTableBody.appendChild(row);
+      });
+    } else {
+      const row = document.createElement("tr");
+      row.innerHTML = `<td colspan="4">No hay datos almacenados.</td>`;
+      datosTableBody.appendChild(row);
+    }
+  }
+
+  mostrarDatosAlmacenados();
