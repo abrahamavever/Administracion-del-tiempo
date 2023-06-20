c6c6d86b665fc4f32f1d974d13882e6dc9878bbf
Abraham Avendaño Vercelli <abrahamavever@gmail.com>
2023-06-20 12:57:30 -0400

    git commit -m "Actualizar index.html y script.js
    Se actualizaron los archivos index.html y script.js para mejorar la estructura y legibilidad del código. Además, se realizaron ajustes en el código para reflejar prácticas de codificación más profesionales. Ahora la página se ve más profesional y se ha mejorado la experiencia del usuario."

--- script.js
+++ script.js
@@ -1,36 +1,65 @@
-// Definir variables para la acción de trotar
 let trotarDuracionInicial = 70;
 let trotarDuracionIncremento = 10;
 let trotarNivel = 1;
 let cuentaRegresiva;
 let sonidoAlerta = new Audio('alerta.mp3');
+let nivelGuardado = localStorage.getItem("nivelTrotar");
 
-// Función para incrementar el nivel de trotar
+if (nivelGuardado) {
+    trotarNivel = parseInt(nivelGuardado);
+    trotarDuracionInicial = 70 + (trotarNivel - 1) * 10;
+} else {
+    guardarNivelTrotar();
+    nivelGuardado = trotarNivel.toString();
+}
+
+actualizarDatosTrotar();
+
+function guardarNivelTrotar() {
+    localStorage.setItem("nivelTrotar", trotarNivel.toString());
+}
+
+function actualizarDatosTrotar() {
+    document.getElementById("trotar-nivel").textContent = trotarNivel;
+    document.getElementById("trotar-duracion").textContent = trotarDuracionInicial;
+}
+
 function aumentarNivelTrotar() {
     trotarNivel++;
     trotarDuracionInicial += trotarDuracionIncremento;
-    document.getElementById("trotar-nivel").textContent = trotarNivel;
-    document.getElementById("trotar-duracion").textContent = trotarDuracionInicial;
-    console.log("¡Has subido de nivel en trotar! Nivel actual: " + trotarNivel);
-    console.log("Nueva duración de trotar: " + trotarDuracionInicial + " segundos");
-  }
+    guardarNivelTrotar();
+    actualizarDatosTrotar();
+    console.log(`¡Has subido de nivel en trotar! Nivel actual: ${trotarNivel}`);
+    console.log(`Nueva duración de trotar: ${trotarDuracionInicial} segundos`);
+}
 
-  function empezarCuentaRegresiva() {
+function bajarNivelTrotar() {
+    if (trotarNivel > 1) {
+        trotarNivel--;
+        trotarDuracionInicial -= trotarDuracionIncremento;
+        guardarNivelTrotar();
+        actualizarDatosTrotar();
+        console.log(`¡Has bajado de nivel en trotar! Nivel actual: ${trotarNivel}`);
+        console.log(`Nueva duración de trotar: ${trotarDuracionInicial} segundos`);
+    }
+}
+
+function empezarCuentaRegresiva() {
     const duracionInicial = parseInt(document.getElementById("trotar-duracion").textContent);
     let segundosRestantes = duracionInicial;
     document.getElementById("trotar-duracion").textContent = segundosRestantes;
-  
-    cuentaRegresiva = setInterval(() => {
-      segundosRestantes--;
-      document.getElementById("trotar-duracion").textContent = segundosRestantes;
-  
-      if (segundosRestantes <= 0) {
-        clearInterval(cuentaRegresiva);
-        reproducirSonidoAlerta();
-      }
-    }, 1000);
-  }
 
-  function reproducirSonidoAlerta() {
+    cuentaRegresiva = setInterval(() => {
+        segundosRestantes--;
+        document.getElementById("trotar-duracion").textContent = segundosRestantes;
+
+        if (segundosRestantes <= 0) {
+            clearInterval(cuentaRegresiva);
+            reproducirSonidoAlerta();
+        }
+    }, 1000);
+}
+
+function reproducirSonidoAlerta() {
     sonidoAlerta.play();
-  }
+}
